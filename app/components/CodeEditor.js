7e8fe3f790f97515cb5a160524e8f35a16942935
Dipankar Majumdar <dipmajumdar1991@gmail.com>
2024-06-20 11:27:42 +0530

    updated

--- app/components/CodeEditor.js
+++ app/components/CodeEditor.js
@@ -39,13 +39,13 @@ const CodeEditor = ({ darkMode }) => {
       <div className="flex space-x-4 p-2">
         <button
           onClick={runCode}
-          className="text-white bg-gradient-to-r from-cyan-600 via-cyan-700 to-cyan-800 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-2"
+          className="text-white bg-gradient-to-r from-cyan-600 via-cyan-700 to-cyan-800 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-2 mb-2"
         >
           Run Code
         </button>
         <button
           onClick={submitCode}
-          className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-2"
+          className="text-white bg-gradient-to-r from-green-600 via-green-700 to-green-800 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-2 mb-2"
         >
           Submit Code
         </button>
