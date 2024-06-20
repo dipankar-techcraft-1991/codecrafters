a64e43b5a5cb469771025faf68244fafc0620407
Dipankar Majumdar <dipmajumdar1991@gmail.com>
2024-06-20 09:20:59 +0530

    updated

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,9 +4,13 @@ const Navbar = ({ darkMode, changeDarkMode }) => {
       <div className="text-xl font-bold text-white">CODE CRAFTERS</div>
       <button
         onClick={changeDarkMode}
-        className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 outline-none"
+        className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 font-medium rounded-lg text-sm px-1 py-1 text-center me-2 mb-2"
       >
-        {darkMode ? "Light Mode" : "Dark Mode"}
+        {darkMode ? (
+          <img src="./light.png" alt="light" height={`30px`} width={`30px`} />
+        ) : (
+          <img src="./dark.png" alt="dark" height={`30px`} width={`30px`} />
+        )}
       </button>
     </nav>
   );
