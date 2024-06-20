6ba075eedfe124b923ac50a32fb7015fd8721673
Dipankar Majumdar <dipmajumdar1991@gmail.com>
2024-06-20 10:34:24 +0530

    updated

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = ({ darkMode, changeDarkMode }) => {
   return (
-    <nav className="flex justify-between items-center p-4 bg-navbar shadow-2xl !mr-0">
-      <div className="font-extrabold text-transparent text-xl bg-clip-text bg-gradient-to-r from-purple-300 to-green-300">
+    <nav className="flex justify-between items-center p-4 bg-gradient-to-r from-slate-900 to-slate-700 shadow-2xl">
+      <div className="font-extrabold text-transparent text-xl bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700">
         CODE CRAFTERS
       </div>
       <button
